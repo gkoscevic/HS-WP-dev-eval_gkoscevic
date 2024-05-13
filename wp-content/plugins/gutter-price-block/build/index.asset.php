136855528a7dd5f977f3ca5ff8051e74e7d90d10
Greg Koscevic <gregory.koscevic@gmail.com>
2024-05-13 14:30:16 -0400

    [feature/gutters-inventory] - - inital setup of gutenberg block - setting up individual gutter page

---
+++ wp-content/plugins/gutter-price-block/build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '3553f3cbeaa3d6b7e7da');
---
+++ wp-content/plugins/gutter-price-block/build/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '67308aa4bf4cf4dc0096');
